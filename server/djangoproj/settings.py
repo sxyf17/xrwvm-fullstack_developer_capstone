bd695e919af5eec76b63036c03c7503ac5e3771c
sxyf17 <farooqisaif1@gmail.com>
2024-08-19 22:57:10 -0400

    finished

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['locals', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'localhost:8000']
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000',]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
